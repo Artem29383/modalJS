7172f6677fda68de8a1caec1a3975eaa22a627eb
Artem29383 <54779368+Artem29383@users.noreply.github.com>
2020-02-21 00:40:06 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const fruits = [
+  {id: 1, title: 'Яблоки', price: 20, img: 'https://e1.edimdoma.ru/data/ingredients/0000/2374/2374-ed4_wide.jpg?1487746348'},
+  {id: 2, title: 'Апельсины', price: 30, img: 'https://fashion-stil.ru/wp-content/uploads/2019/04/apelsin-ispaniya-kg-92383155888981_small6.jpg'},
+  {id: 3, title: 'Манго', price: 40, img: 'https://itsfresh.ru/upload/iblock/178/178d8253202ef1c7af13bdbd67ce65cd.jpg'},
+];
+
+const content = document.querySelector('.container');
+const contentFruits = content.querySelector('.row');
+contentFruits.addEventListener('click', showMePrice);
+
+(function () {
+  const row = document.createElement('div');
+  row.classList.add('row');
+  fruits.forEach(fruit => {
+    const col = document.createElement('div');
+    col.classList.add('col');
+    col.innerHTML = `<div class="col">
+      <div class="card">
+        <img class="card-img-top" style="height: 300px;" src=${fruit.img}>
+        <div class="card-body">
+          <h5 class="card-title">${fruit.title}</h5>
+          <a href="#" class="btn btn-primary" data-id = ${fruit.id}>Посмотреть цену</a>
+          <a href="#" class="btn btn-danger">Удалить</a>
+        </div>
+      </div>
+    </div>
+`;
+    row.append(col);
+  });
+  content.append(row);
+}());
+
+function showMePrice(e) {
+  const id = e.target.dataset.id;
+  modal.setContent(`
+  <h4>Цена ${fruits[id-1].price}</h4>
+  `);
+  modal.open();
+}
+
+const modal = $.modal({
+  title: 'My modal',
+  content: `<h4>Modal is open</h4>
+          <p>Lorem ipsum dolor sit amet.</p>`,
+  closable: true,
+  width: '400px',
+  footerButtons: [
+    {
+      text: 'Ок',
+      type: 'primary',
+      handler() {
+        modal.close()
+      }
+    },
+    {
+      text: 'Cancel',
+      type: 'danger',
+      handler() {
+        modal.close()
+      }
+    }
+  ]
+});
+
+
+
+
+
+
+
---
+++ plugins/modal.js
@@ -0,0 +1,86 @@
+Element.prototype.appendAfter = function (element) {
+  element.parentNode.insertBefore(this, element.nextSibling);
+};
+
+function noop() {}//глушилка
+
+function _createModalFooter(buttons = []) {
+  if (buttons.length === 0) return false;
+  
+  const wrap = document.createElement('div');
+  wrap.classList.add('modal-footer');
+  
+  buttons.forEach(btn => {
+    const $btn = document.createElement('button');
+    $btn.textContent = btn.text;
+    $btn.classList.add('btn');
+    $btn.classList.add(`btn-${btn.type || 'secondary'}`);
+    $btn.onclick = btn.handler || noop;
+    
+    wrap.append($btn);
+  });
+  
+  return wrap;
+}
+
+function _createModal(options) {
+  const modal = document.createElement('div');
+  modal.classList.add('vmodal');
+  modal.insertAdjacentHTML('afterbegin', `
+  <div class="modal-overlay" data-close = "true">
+      <div class="modal-window">
+        <div class="modal-header">
+          <span class="modal-title">${options.title || 'Modal'}</span>
+          ${options.closable ? '<span class="modal-close" data-close = "true">&times;</span>' : ''}
+        </div>
+        <div class="modal-body" data-content>
+          ${options.content || ''}
+        </div>
+      </div>
+  </div>
+  `);
+  modal.style.width = options.width || '400px';
+  const footer = _createModalFooter(options.footerButtons);
+  footer.appendAfter(modal.querySelector("[data-content]"));
+  document.body.append(modal);
+  return modal;
+}
+
+$.modal = function (options) {
+  const ANIMATION_SPEED = 200;
+  const $modal = _createModal(options);
+  let closing = false; //чтоб не открыть модальное окно пока оно полностью не закроется
+  
+  const modal = {
+    open() {
+      !closing && $modal.classList.add('open');
+    },
+    close() {
+      closing = true;
+      $modal.classList.remove('open');
+      $modal.classList.add('hide');
+      setTimeout(() => {
+        $modal.classList.remove('hide');
+        closing = false;
+      }, ANIMATION_SPEED);
+    }
+  };
+  
+  const listener = e => {
+    if (e.target.dataset.close) {
+      modal.close();
+    }
+  };
+  
+  $modal.addEventListener('click', listener);
+  
+  return Object.assign(modal, {
+    destroy() {
+      $modal.removeEventListener('click', listener);
+      $modal.remove();
+    },
+    setContent(html) {
+      $modal.querySelector('[data-content]').innerHTML = html;
+    }
+  })
+};
